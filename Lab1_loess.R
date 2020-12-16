70a96daa0a6c08bec6339df7827886a1abb59099
Zihan9710 <71140473+Zihan9710@users.noreply.github.com>
2020-12-17 00:20:40 +0800

    Add files via upload

---
+++ Lab1_loess.R
@@ -0,0 +1,26 @@
+#---------------------loess1-------------------------------
+data(cars)
+cars.lo <- loess(dist ~ speed, cars)
+predict(cars.lo, data.frame(speed = seq(5, 30, 1)), se = TRUE)
+# to allow extrapolation
+cars.lo2 <- loess(dist ~ speed, cars, control = loess.control(surface = "direct"))
+predict(cars.lo2, data.frame(speed = seq(5, 30, 1)), se = TRUE)
+
+#---------------------loess2-------------------------------
+require(graphics)
+
+plot(cars, main = "lowess(cars)")
+lines(lowess(cars), col = 2)
+lines(lowess(cars, f = .2), col = 3)
+legend(5, 120, c(paste("f = ", c("2/3", ".2"))), lty = 1, col = 2:3)
+
+#---------------------loess3-------------------------------
+require(graphics)
+
+with(cars, {
+  plot(speed, dist)
+  lines(supsmu(speed, dist))
+  lines(supsmu(speed, dist, bass = 7), lty = 2)
+})
+
+
